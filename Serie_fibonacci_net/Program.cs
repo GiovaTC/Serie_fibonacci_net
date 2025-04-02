93ea6e5b48f649a5f92321be7726e7ba2a571541
gtapiero <giovanny.tapiero@pi.edu.co>
2025-04-02 16:09:18 -0500

    Agregar archivos de proyecto.

---
+++ Serie_fibonacci_net/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la cantidad de términos de la serie Fibonacci: ");
+        if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+        {
+            Fibonacci(n);
+        }
+        else
+        {
+            Console.WriteLine("Por favor, ingrese un numero entero positivo");
+        }
+    }
+
+    static void Fibonacci(int n)
+    {
+        long a = 0, b = 1, temp;
+
+        Console.WriteLine($"Serie Fibonacci hasta {n} terminos:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(a + " ");
+            temp = a;
+            a = b;
+            b = temp + b;
+        }
+        Console.WriteLine();
+    }
+}
+                
+    
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+               
+
